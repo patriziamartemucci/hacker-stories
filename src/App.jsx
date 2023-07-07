03565c9ad0a2068200b8c62b5e27c3b6badf5fda
patriziamartemucci <martemucci.patrizia@gmail.com>
2023-07-07 16:40:17 +0200

    7 luglio

--- src/App.jsx
+++ src/App.jsx
@@ -16,34 +16,42 @@ const list =[{
   objectID:1
 }
 ];
+function List(){
+  return(
+  <ul>
+    {
+    list.map(function(item){
+      return (
+      <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+        </li>
+        );
+      })
+    }
+  </ul>);
+}
 
-const numbers = [1,2,3,4,5];
-const esponenziale=numbers.map(function(number){
-  return number*number;
-});
-console.log(esponenziale);
+function Search(){
+  return(
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
 
 function App(){
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <Search />
       <hr />
-      <ul>
-        {
-        list.map(function(item){
-          return <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            </li>;
-        })
-      }
-      </ul>
+      <List />
     </div>
   );
 }
