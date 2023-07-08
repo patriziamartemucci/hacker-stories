ab8e6412de93ef672f2de0db72fdecc5d02f8c13
patriziamartemucci <martemucci.patrizia@gmail.com>
2023-07-08 16:48:25 +0200

    commit props e altro

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,73 @@
 import * as React from 'react';
-const list =[{
-  title:'React',
-  url:'https://reactjs.org',
-  author:'Jordan Walke',
-  num_comments:3,
-  points:4,
-  objectID:0
-},
-{
-  title:'Redux',
-  url:'https://reducs.js.org',
-  author:'Dan Abramov, Andrew Clark',
-  num_comments:2,
-  points:5,
-  objectID:1
-}
-];
-function List(){
-  return(
+
+const List = (props) => (
   <ul>
-    {
-    list.map(function(item){
-      return (
-      <li key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-        </li>
-        );
-      })
-    }
-  </ul>);
-}
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+);
 
-function Search(){
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
+);
+
+
+const Search = () => {
+  const [searchTerm, setSearchTerm]=React.useState('');
+  const handleChange=(event)=>{
+    setSearchTerm(event.target.value);
+  };
   return(
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
-    </div>
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" onChange={handleChange}/>
+    <p>
+      Searching for <strong> {searchTerm}</strong>
+    </p>
+  </div>
   );
-}
+};
 
-function App(){
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
-      <Search />
-      <hr />
-      <List />
-    </div>
+const App = () => {
+
+  const stories =[{
+    title:'React',
+    url:'https://reactjs.org',
+    author:'Jordan Walke',
+    num_comments:3,
+    points:4,
+    objectID:0
+  },
+  {
+    title:'Redux',
+    url:'https://reducs.js.org',
+    author:'Dan Abramov, Andrew Clark',
+    num_comments:2,
+    points:5,
+    objectID:1
+  }
+  ];
+  
+  return(
+  <div>
+    <h1>My Hacker Stories</h1>
+
+    <Search />
+
+    <hr />
+
+    <List list={stories} />
+  </div>
   );
-}
+};
+
 
 export default App;
