be0dbb030bf19ed2dd7a2d6e4934f6de8a0ac3a6
patriziamartemucci <martemucci.patrizia@gmail.com>
2023-07-15 17:50:23 +0200

    React custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,18 @@ const Search = ({search, onSearch}) => (
   </div> 
 );
 
+const useStorageState = (key,initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+
+  React.useEffect(() => {
+  localStorage.setItem(key,value);
+  },[value,key]);
+  return [value,setValue];
+};
+
 const App = () => {
 
   const stories =[{
@@ -48,12 +60,9 @@ const App = () => {
   }
   ];
 
-  const [searchTerm, setSearchTerm]=React.useState(
-    localStorage.getItem('search') || 'React');
-
-  React.useEffect(() => {
-    localStorage.setItem('search',searchTerm);
-  },[searchTerm]);
+  const [searchTerm, setSearchTerm]=useStorageState(
+    'search','React'
+  );
   
   const handleSearch=(event)=>{
     setSearchTerm(event.target.value);
