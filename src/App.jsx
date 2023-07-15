12ef9a646d0ca9211a3bd34576b11c4862845f8d
patriziamartemucci <martemucci.patrizia@gmail.com>
2023-07-15 17:02:39 +0200

    React side-effect

--- src/App.jsx
+++ src/App.jsx
@@ -48,11 +48,17 @@ const App = () => {
   }
   ];
 
-  const [searchTerm, setSearchTerm]=React.useState('React');
+  const [searchTerm, setSearchTerm]=React.useState(
+    localStorage.getItem('search') || 'React');
+
+  React.useEffect(() => {
+    localStorage.setItem('search',searchTerm);
+  },[searchTerm]);
+  
   const handleSearch=(event)=>{
     setSearchTerm(event.target.value);
     console.log(event.target.value);
-   // props.onSearch(event);
+    localStorage.setItem('search',event.target.value);
   };
 
   const searchedStories = stories.filter((story) => 
