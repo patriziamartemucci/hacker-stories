76db2c41f43581fc4a8908336711142355306ca9
patriziamartemucci <martemucci.patrizia@gmail.com>
2023-07-16 16:45:34 +0200

    React Fragment

--- src/App.jsx
+++ src/App.jsx
@@ -22,10 +22,10 @@ const Item = ({item}) => (
 
 
 const Search = ({search, onSearch}) => (
- <div>
+ <>
     <label htmlFor="search">Search: </label>
     <input id="search" type="text" value={search} onChange={onSearch}/>  
-  </div> 
+  </> 
 );
 
 const useStorageState = (key,initialState) => {
