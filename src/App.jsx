0899f59b0d38dcc2204139b9fac9f5a177bf05d8
patriziamartemucci <martemucci.patrizia@gmail.com>
2023-07-16 17:14:08 +0200

    Reusable React Component

--- src/App.jsx
+++ src/App.jsx
@@ -21,10 +21,11 @@ const Item = ({item}) => (
 
 
 
-const Search = ({search, onSearch}) => (
+const InputWithLabel = ({id,label,value,type='text',onInputChange}) => (
  <>
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" value={search} onChange={onSearch}/>  
+    <label htmlFor={id}>{label} </label>
+    &nbsp;
+    <input id={id} type={type} value={value} onChange={onInputChange}/>  
   </> 
 );
 
@@ -79,7 +80,13 @@ const App = () => {
   <div>
     <h1>My Hacker Stories</h1>
 
-    <Search  search={searchTerm} onSearch={handleSearch}/>
+    <InputWithLabel
+    id="search"
+    label="Search"
+    value={searchTerm}
+    type="text"
+    onInputChange={handleSearch}
+    />
 
     <hr />
 
