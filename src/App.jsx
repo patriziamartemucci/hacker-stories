6ba3bcd1eefa57fefbb99f97b95e8683db7da12f
patriziamartemucci <martemucci.patrizia@gmail.com>
2023-07-17 16:20:49 +0200

    React component composition

--- src/App.jsx
+++ src/App.jsx
@@ -21,9 +21,9 @@ const Item = ({item}) => (
 
 
 
-const InputWithLabel = ({id,label,value,type='text',onInputChange}) => (
+const InputWithLabel = ({id,label,value,type='text',onInputChange,children}) => (
  <>
-    <label htmlFor={id}>{label} </label>
+    <label htmlFor={id}>{children} </label>
     &nbsp;
     <input id={id} type={type} value={value} onChange={onInputChange}/>  
   </> 
@@ -82,12 +82,11 @@ const App = () => {
 
     <InputWithLabel
     id="search"
-    label="Search"
     value={searchTerm}
-    type="text"
     onInputChange={handleSearch}
-    />
-
+    >
+      <strong>Search:</strong>
+    </InputWithLabel>
     <hr />
 
     <List list={searchedStories} />
